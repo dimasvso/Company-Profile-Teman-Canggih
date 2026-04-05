3230c6b2a6f1b74b07701d4f4d0a98f5726f947b
Dimas <susilodim@gmail.com>
2026-04-05 20:55:43 +0700

    feat: add router

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter } from "react-router-dom";
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
+import { router } from "./routes/index.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
