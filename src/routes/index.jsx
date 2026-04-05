590641475a8d183bbf7eb60b1897757fc915fb12
Dimas <susilodim@gmail.com>
2026-04-05 20:00:19 +0700

    feat: add Router and file page

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,14 +1,27 @@
 import { createBrowserRouter } from "react-router-dom";
+import Template from "../component/Template";
+import Home from "../pages/Home";
+import About from "../pages/About";
+import Contact from "../pages/Contact";
+
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Template />,
     children: [
-        {
-
-        },
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
     ],
   },
-
 ]);
